beccd6f748f311a3601c8231ecaedcb3ad63f2cc
Peter Reid <peter.d.reid@gmail.com>
2012-10-10 17:13:41 -0400

    Set up SAX parser callbacks

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 node-xml-collector
 ==================
+
+Makes SAX parsing less painful. This handles only what I arbitrarily consider a
+sane subset of XML.
--- index.js
+++ index.js
@@ -3,8 +3,37 @@ var SaxParser = require('node-xml').SaxParser;
 var EventEmitter = require("events").EventEmitter;
 
 var XmlCollector = exports = module.exports = function() {
+  var self = this;
   EventEmitter.call(this);
   
   
+  this.parser = new SaxParser(function(cb) {
+    cb.onStartElementNS(self.onStartElement.bind(self));
+    cb.onEndElementNS(self.onEndElement.bind(self));
+    cb.onCharacters(self.onCharacters.bind(self));
+    cb.onWarning(self.onWarning.bind(self));
+    cb.onError(self.onError.bind(self));
+  });
 };
+
+XmlCollector.prototype.onStartElement = function(elem, attrs) {
+
+};
+
+XmlCollector.prototype.onEndElement = function(elem) {
+
+};
+
+XmlCollector.prototype.onCharacters = function(str) {
+
+};
+
+XmlCollector.prototype.onWarning = function(msg) {
+  self.emit('warning', new Error(msg));
+};
+
+XmlCollector.prototype.onError = function(msg) {
+  self.emit('error', new Error(msg));
+};
+
 util.inherits(XmlCollector, EventEmitter);
