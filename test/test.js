b93bc958fa08d7120c7f196a982ed3fe9e68716e
Peter Reid <peter.d.reid@gmail.com>
2012-10-10 18:28:17 -0400

    Test actually collects some data
    Text isn't handled yet, but otherwise the test is working. It takes the
    XML document representing a figure with lines with points with x,y and
    turns it into an Object. I am using 'emit' to get collected data out,
    which was not my plan but I think is actually cleaner. Emitting things
    whenever they are ready should be easier to connect to other streams.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,43 @@
 var fs = require('fs');
 var XmlCollector = require('../index');
 
-fs.createReadStream('./small.xml').pipe(new XmlCollector({}))
+function Point() {
+}
+function Line() {
+  this.points = [];
+}
+
+fs.createReadStream('./small.xml').pipe(new XmlCollector({
+  children: {
+    'figure': {
+      enter: function() { return []; },
+      children: {
+        'line': {
+          enter: function() { return new Line() },
+          children: {
+            'point': {
+              enter: function() { return new Point(); },
+              children: {
+                'x': {
+                  enter: function() { return ''; },
+                  text: function(text, additional) { text += additional; },
+                  exit: function(point, text) { point.x = text; }
+                },
+                'y': {
+                  enter: function() { return ''; },
+                  text: function(text, additional) { text += additional; },
+                  exit: function(point, text) { point.y = text ; }
+                }
+              },
+              exit: function(line, point) { line.points.push(point); }
+            }
+          },
+          exit: function(figure, line) { figure.push(line); }
+        }
+      },
+      exit: function(ctx, shapes) { this.emit('figure', shapes); }
+    }
+  }
+})).on('figure', function(figure) {
+  console.log('figure:', JSON.stringify(figure));
+});
