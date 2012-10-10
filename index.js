820dc084125a978808f2569cb1e3144189286bbe
Peter Reid <peter.d.reid@gmail.com>
2012-10-10 17:03:57 -0400

    Inherit from EventEmitter

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var util = require('util');
 var SaxParser = require('node-xml').SaxParser;
-
+var EventEmitter = require("events").EventEmitter;
 
 var XmlCollector = exports = module.exports = function() {
+  EventEmitter.call(this);
   
 }
+util.inherits(XmlCollector, EventEmitter);
